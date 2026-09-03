3e21c4ff7d0e429fec1a4ef50fc576cf7aa0d707
Agnieszka Lenik <agnieszkalenik@Mac.lan>
2026-09-03 13:35:59 +0100

    commit 03/09

--- src/app/views/Customers.jsx
+++ src/app/views/Customers.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from 'react'
+import './Customers.css'
+import axios from "axios"
+
 const Customers = () => {
 
-    return(
-        <div className="xx">xx</div>
+    const [customers, setCustomers] = useState([])
+
+    const listCustomers = () => {
+        axios
+        .get("http://localhost:8080/customer")
+        .then((response)=>{
+            setCustomers(response.data)
+            console.log(response)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+    }
+
+       useEffect(()=>{
+        listCustomers()
+    },[])
+
+    const listCustomer = customers.map((customer)=>{
+        <tr>
+                <th scope='row'>{customer.name}</th>
+                <th scope='row'>{customer.tier}</th>
+                <th scope='row'>{customer.representative.name}</th>
+                <th scope='row'>{customer.representative.contact}</th>
+                <th scope='row'>{customer.country}</th>
+                <th scope='row'>{customer.zone}</th>
+                <th scope='row'><button>Delete Customer</button></th>
+            </tr>
+         })
+
+
+    return (
+       <table>
+        <caption>Customers list</caption>
+        <thead>
+            <tr>
+                <th scope='col'>Customer</th>
+                <th scope='col'>Tier</th>
+                <th scope='col'>Representative</th>
+                <th scope='col'>Representative's contact</th>
+                <th scope='col'>Country</th>
+                <th scope='col'>zone</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {listCustomer}
+        </tbody>
+       </table>
     )
 }
 
